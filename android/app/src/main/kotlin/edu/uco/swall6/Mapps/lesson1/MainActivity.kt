d5177c4f5be53123bcd6125d85297915cf0994e6
SeanAlexWall <seanalexwall@gmail.com>
2021-01-26 20:18:35 -0600

    init

---
+++ android/app/src/main/kotlin/edu/uco/swall6/Mapps/lesson1/MainActivity.kt
@@ -0,0 +1,6 @@
+package edu.uco.swall6.Mapps.lesson1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Lesson1App());
+}
+
+class Lesson1App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Lesson1 App'),
+    );
+  }
+}
